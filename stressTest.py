b1429b936064bad876a834e3458509b10367a611
Leonardo Nhemetz <leonardo@inovattion.com.br>
2023-03-28 15:02:31 -0300

    aaa

--- stressTest.py
+++ stressTest.py
@@ -1,17 +1,19 @@
 # import the required libraries
-import mysql.connector
+import pymysql
 import time
 
 # define the MySQL connection settings
-mydb = mysql.connector.connect(
+mydb = pymysql.connect(
   host="category-app-mysql",
   user="root",
   password="root",
-  database="cad_imobiliarias"
+  database="cad_imobiliarias",
+  port=81,  # port number for the MySQL server
+  autocommit=True
 )
 
 # define the query to be executed
-query = "SELECT * FROM cadastros"
+query = "SELECT * FROM yourtable"
 
 # define the number of times to execute the query
 num_iterations = 100
@@ -19,12 +21,12 @@ num_iterations = 100
 # execute the query for the given number of times
 start_time = time.time() # start timer
 for i in range(num_iterations):
-    mycursor = mydb.cursor()
-    mycursor.execute(query)
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
+    with mydb.cursor() as cursor:
+        cursor.execute(query)
+        myresult = cursor.fetchall()
+        for x in myresult:
+            print(x)
 end_time = time.time() # end timer
 
-# calculate and print the time taken to execute the querie
+# calculate and print the time taken to execute the queries
 print("Time taken to execute the query " + str(num_iterations) + " times: " + str(end_time - start_time) + " seconds")
